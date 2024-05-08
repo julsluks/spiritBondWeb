beace553841f221fa99d731bc3bebc6dc70c43c1
julsluks <a22betvilver@inspedralbes.cat>
2024-05-08 23:19:55 +0200

    chore: Update dev script to use port 3001 for local development

--- nuxt-spiritBond/pages/aboutUs.vue
+++ nuxt-spiritBond/pages/aboutUs.vue
@@ -1,19 +1,65 @@
 <template>
-    <div>
-
+    <div class="h-screen w-full bg-gradient-to-br from-blue-600 to-blue-950 flex flex-col items-center justify-center text-white">
+        <div class="flex flex-inline items-center justify-center">
+            <h1 class="text-6xl font-serif font-bold tracking-widest mb-4" style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">ABOUT US</h1>
+            <img class="h-[225px] w-[150px] ml-3" src="assets/aboutUs/logo_studio.png">
+        </div>
+        <div class="flex flex-inline">
+            <!-- presentacion grupo -->
+            <div class="flex flex-col">
+                <h2>PROGRAMMERS</h2>
+                <div class="flex flex-inline">
+                    <div>
+                        <h3>NOMBRE</h3>
+                        <img src="" alt="imagen">
+                        <ul>
+                            <li>INTELIGENTE</li>
+                            <li>RESPONSABLE</li>
+                            <li>AMIGABLE</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>NOMBRE</h3>
+                        <img src="" alt="imagen">
+                        <ul>
+                            <li>INTELIGENTE</li>
+                            <li>RESPONSABLE</li>
+                            <li>AMIGABLE</li>
+                        </ul>
+                    </div>
+                </div>
+                <h2>ARTISTS</h2>
+                <div class="flex flex-inline">
+                    <div>
+                        <h3>NOMBRE</h3>
+                        <img src="" alt="imagen">
+                        <ul>
+                            <li>INTELIGENTE</li>
+                            <li>RESPONSABLE</li>
+                            <li>AMIGABLE</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>NOMBRE</h3>
+                        <img src="" alt="imagen">
+                        <ul>
+                            <li>INTELIGENTE</li>
+                            <li>RESPONSABLE</li>
+                            <li>AMIGABLE</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col">
+                div*
+            </div>
+            <div class="flex flex-col">
+                div*
+            </div>
+        </div>
     </div>
 </template>
 
-<script>
-export default {
-    setup () {
-        
-
-        return {}
-    }
-}
-</script>
-
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
--- nuxt-spiritBond/pages/index.vue
+++ nuxt-spiritBond/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-background-image flex flex-col items-center justify-center h-screen text-white">
-        <h1 class="text-6xl font-serif font-bold tracking-widest mb-4">SPIRIT BOUND</h1>
+        <h1 class="text-6xl font-serif font-bold tracking-widest mb-4" style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">SPIRIT BOUND</h1>
         <h4 class="text-xl font-serif font-extralight tracking-widest text-center">
             <!-- <span class="text-sm inline-block align-middle">&lt;&lt;</span>--> SURVIVE AND ESCAPE
             <!-- <span class="text-sm inline-block align-middle">&gt;&gt;</span>-->
@@ -18,7 +18,7 @@
             <h2 class="text-center text-5xl font-serif tracking-widest -mt-9 pb-9"
                 style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">SPIRIT BOUND</h2>
             <div class="inline-grid grid-cols-2 gap-3 lg:gap-9 mt-4">
-                <div class="w-[300px] m-3 lg:m-6">
+                <div class="max-w-[300px] m-3 lg:m-6">
                     <h3 class="text-2xl font-serif tracking-widest pb-3"
                         style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">DESCRIPTION</h3>
                     <p class="text-base">KB is a boy of dwarf race who
@@ -26,7 +26,7 @@
                         giants have taken away his sister and
                         You will have to find a way to meet her.</p>
                 </div>
-                <div class="w-[300px] m-3 lg:m-6">
+                <div class="max-w-[300px] m-3 lg:m-6">
                     <h3 class="text-2xl font-serif tracking-widest pb-3"
                         style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">ABOUT THE STUDY</h3>
                     <p class="text-base">Studio Shonos is a studio founded in
@@ -35,7 +35,7 @@
                         will present its first version on May 12
                         2024 after 5 months of development.</p>
                 </div>
-                <div class="w-[300px] m-3 lg:m-6">
+                <div class="max-w-[300px] m-3 lg:m-6">
                     <h3 class="text-2xl font-serif tracking-widest pb-3"
                         style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">THE GAME</h3>
                     <p class="text-base">SpiritBound is an action and puzzle game
@@ -47,7 +47,7 @@
                         Various control inputs including
                         Xbox Controller and Keyboard + Mouse</p>
                 </div>
-                <div class="w-[300px] m-3 lg:m-6">
+                <div class="max-w-[300px] m-3 lg:m-6">
                     <h3 class="text-2xl font-serif tracking-widest pb-3"
                         style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">PEGI</h3>
                     <div class="inline-grid grid-cols-2 gap-5 m-3">
@@ -62,7 +62,7 @@
         </div>
         <div class="h-screen lg:w-1/2 w-full flex flex-col items-center justify-center">
             <div class="inline-grid grid-cols-2 gap-3">
-                <div class="w-[300px] h-[175px] bg-gradient-to-br from-blue-600 to-black p-4">
+                <div class="min-w-[300px] h-[175px] bg-gradient-to-br from-blue-600 to-black p-4">
                     <h3 class="text-2xl font-serif tracking-widest pb-3"
                         style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">KB</h3>
                     <p class="text-base ml-3">
@@ -71,13 +71,13 @@
                         DWARF
                     </p>
                 </div>
-                <div class="w-[300px] h-[175px]">
+                <div class="min-w-[300px] h-[175px]">
                     <img src="" alt="KB">
                 </div>
-                <div class="w-[300px] h-[175px]">
+                <div class="min-w-[300px] h-[175px]">
                     <img src="" alt="DESSIE">
                 </div>
-                <div class="w-[300px] h-[175px] bg-gradient-to-br from-blue-600 to-black p-4">
+                <div class="min-w-[300px] h-[175px] bg-gradient-to-br from-blue-600 to-black p-4">
                     <h3 class="text-2xl font-serif tracking-widest pb-3"
                         style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">DESSIE</h3>
                     <p class="text-base ml-3">
@@ -87,7 +87,7 @@
                         DWARF
                     </p>
                 </div>
-                <div class="w-[300px] h-[175px] bg-gradient-to-br from-blue-600 to-black p-4">
+                <div class="min-w-[300px] h-[175px] bg-gradient-to-br from-blue-600 to-black p-4">
                     <h3 class="text-2xl font-serif tracking-widest pb-3"
                         style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">GREN</h3>
                     <p class="text-base ml-3">
@@ -97,13 +97,13 @@
                         GIANT
                     </p>
                 </div>
-                <div class="w-[300px] h-[175px]">
+                <div class="min-w-[300px] h-[175px]">
                     <img src="" alt="GREN">
                 </div>
-                <div class="w-[300px] h-[175px]">
+                <div class="min-w-[300px] h-[175px]">
                     <img src="" alt="ANTON">
                 </div>
-                <div class="w-[300px] h-[175px] bg-gradient-to-br from-blue-600 to-black p-4">
+                <div class="min-w-[300px] h-[175px] bg-gradient-to-br from-blue-600 to-black p-4">
                     <h3 class="text-2xl font-serif tracking-widest pb-3"
                         style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">ANTON</h3>
                     <p class="text-base ml-3">
