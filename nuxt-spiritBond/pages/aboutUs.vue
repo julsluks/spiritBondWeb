92790e712a3d273bf7d87908f5bb705f8b2974d5
julsluks <a22betvilver@inspedralbes.cat>
2024-05-09 23:11:18 +0200

    Refactor aboutUs.vue layout and styles

--- nuxt-spiritBond/pages/aboutUs.vue
+++ nuxt-spiritBond/pages/aboutUs.vue
@@ -1,60 +1,94 @@
 <template>
-    <div class="h-screen w-full bg-gradient-to-br from-blue-600 to-blue-950 flex flex-col items-center justify-center text-white">
-        <div class="flex flex-inline items-center justify-center">
+    <div class="min-h-screen w-full bg-gradient-to-br from-blue-600 to-blue-950 flex flex-col items-center justify-center text-white">
+        <div class="flex flex-inline items-center justify-center mt-9 py-4">
             <h1 class="text-6xl font-serif font-bold tracking-widest mb-4" style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">ABOUT US</h1>
             <img class="h-[225px] w-[150px] ml-3" src="assets/aboutUs/logo_studio.png">
         </div>
-        <div class="flex flex-inline">
+        <div class="flex flex-inline text-center mx-auto">
             <!-- presentacion grupo -->
-            <div class="flex flex-col">
-                <h2>PROGRAMMERS</h2>
-                <div class="flex flex-inline">
+            <div class="flex flex-col items-center mb-9 lg:w-1/3 mx-auto">
+                <h2 class="text-center text-3xl font-serif tracking-widest mt-9 pb-9"
+                style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">PROGRAMMERS</h2>
+                <div class="flex flex-inline text-center">
                     <div>
-                        <h3>NOMBRE</h3>
-                        <img src="" alt="imagen">
+                        <h3 class="font-serif tracking-widest">Àlvaro Miguel</h3>
+                        <img class="max-h-[200px] max-w-[150px] mx-auto" src="assets/aboutUs/Àlvaro_(DAMVI).png" alt="Àlvaro Miguel">
+                        <div class="border-b border-gray-300 max-w-[100px] mt-4 mx-auto"></div>
                         <ul>
-                            <li>INTELIGENTE</li>
-                            <li>RESPONSABLE</li>
-                            <li>AMIGABLE</li>
+                            <li>Leader</li>
+                            <li>Programmer</li>
                         </ul>
                     </div>
                     <div>
-                        <h3>NOMBRE</h3>
-                        <img src="" alt="imagen">
+                        <h3 class="font-serif tracking-widest">Luís José</h3>
+                        <img class="max-h-[200px] max-w-[150px] mx-auto" src="assets/aboutUs/Luís José_(DAMVI).png" alt="Luís José">
+                        <div class="border-b border-gray-300 max-w-[100px] mt-4 mx-auto"></div>
                         <ul>
-                            <li>INTELIGENTE</li>
-                            <li>RESPONSABLE</li>
-                            <li>AMIGABLE</li>
+                            <li>Leader</li>
+                            <li>Programmer</li>
                         </ul>
                     </div>
                 </div>
-                <h2>ARTISTS</h2>
-                <div class="flex flex-inline">
+                <h2 class="text-center text-3xl font-serif tracking-widest mt-9 pb-9"
+                style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">ARTISTS</h2>
+                <div class="flex flex-inline text-center">
                     <div>
-                        <h3>NOMBRE</h3>
-                        <img src="" alt="imagen">
+                        <h3 class="font-serif tracking-widest">Judith Pascal</h3>
+                        <img class="max-h-[200px] max-w-[150px] mx-auto" src="assets/aboutUs/Judith_(A3D).png" alt="Judith Pascal">
+                        <h4 class="font-serif tracking-widest text-lg">Directora de Arte</h4>
+                        <div class="border-b border-gray-300 max-w-[100px] mt-2 mb-2 mx-auto"></div>
                         <ul>
-                            <li>INTELIGENTE</li>
-                            <li>RESPONSABLE</li>
-                            <li>AMIGABLE</li>
+                            <li>3D Artist</li>
+                            <li>Artist of <br> Characters and Props</li>
+                            <li>2D/3D animator <br> + Rigger</li>
+                            <li>Conceptual Artist</li>
+                            <li>Video Editor</li>
+                            <li>Administrator of <br> social networks</li>
                         </ul>
                     </div>
                     <div>
-                        <h3>NOMBRE</h3>
-                        <img src="" alt="imagen">
+                        <h3 class="font-serif tracking-widest">Danna Rodríguez</h3>
+                        <img class="max-h-[200px] max-w-[150px] mx-auto" src="assets/aboutUs/Danna_(A3D).png" alt="Danna Rodríguez">
+                        <h4 class="font-serif tracking-widest text-lg">3D Artist</h4>
+                        <div class="border-b border-gray-300 max-w-[100px] mt-2 mb-2 mx-auto"></div>
                         <ul>
-                            <li>INTELIGENTE</li>
-                            <li>RESPONSABLE</li>
-                            <li>AMIGABLE</li>
+                            <li>Conceptual Artist</li>
+                            <li>Artist of Characters <br> (animals) enemies <br> and Props</li>
+                            <li>2D/3D animator <br>+ Rigger</li>
+                            <li>Administrator of <br> social networks</li>
                         </ul>
                     </div>
                 </div>
             </div>
-            <div class="flex flex-col">
-                div*
+            <div class="border-r mx-4 my-3"></div>
+            <div class="flex flex-col items-center mb-9 lg:w-1/3 mx-auto">
+                <h2 class="text-center text-3xl font-serif tracking-widest mt-9 pb-9"
+                style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">PRODUCTION NOTES</h2>
+                <div></div>
+                <h2 class="text-center text-3xl font-serif tracking-widest mt-9 pb-9"
+                style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">MONETIZATION</h2>
+                <div></div>
+                <h2 class="text-center text-3xl font-serif tracking-widest mt-9 pb-9"
+                style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">ABOUT STUDIO SHONOS</h2>
+                <div></div>
             </div>
-            <div class="flex flex-col">
-                div*
+            <div class="border-r mx-4 my-3"></div>
+            <div class="flex flex-col items-center mb-9 lg:w-1/3 mx-auto">
+                <h2 class="text-center text-3xl font-serif tracking-widest mt-9 pb-9"
+                style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">DEVELOPERS</h2>
+                <div></div>
+                <h2 class="text-center text-3xl font-serif tracking-widest mt-9 pb-9"
+                style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">RELEASE DATES</h2>
+                <div></div>
+                <h2 class="text-center text-3xl font-serif tracking-widest mt-9 pb-9"
+                style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">PLATFORMS</h2>
+                <div></div>
+                <h2 class="text-center text-3xl font-serif tracking-widest mt-9 pb-9"
+                style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">YOUTUBE CHANNEL</h2>
+                <div></div>
+                <h2 class="text-center text-3xl font-serif tracking-widest mt-9 pb-9"
+                style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">PRICES</h2>
+                <div></div>
             </div>
         </div>
     </div>
