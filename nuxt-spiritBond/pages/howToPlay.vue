3e94af9790379f526f753506369c0957e38a9c20
julsluks <a22betvilver@inspedralbes.cat>
2024-05-10 19:21:06 +0200

    feat: Add heading to howToPlay page

--- nuxt-spiritBond/pages/howToPlay.vue
+++ nuxt-spiritBond/pages/howToPlay.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="min-h-screen w-full bg-gradient-to-br from-blue-600 to-blue-950 flex flex-col items-center justify-center text-white">
         <h1 class="text-6xl font-serif font-bold tracking-widest mb-4" style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">HOW TO PLAY</h1>
+        
     </div>
 </template>
