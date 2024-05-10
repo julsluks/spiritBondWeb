b4352ba35e781586a3ef2812f5aa31d262b33aed
julsluks <a22betvilver@inspedralbes.cat>
2024-05-10 19:01:15 +0200

    feat: Update FontAwesome dependency to version 6.5.2

--- nuxt-spiritBond/pages/contact.vue
+++ nuxt-spiritBond/pages/contact.vue
@@ -1,19 +1,49 @@
 <template>
-    <div>
-
+    <div
+        class="min-h-screen w-full bg-gradient-to-br from-blue-600 to-blue-950 flex flex-col items-center justify-center text-white">
+        <h1 class="text-6xl font-serif font-bold tracking-widest mb-9 pb-9"
+            style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">SOCIAL NETWORKS</h1>
+        <div class="mt-9 py-9"></div>
+        <div class="relative w-full flex flex-col items-center justify-center text-center mt-9 pt-9 space-x-8">
+            <div class="bg-gray-700 rounded-lg p-6 shadow-xl" style="z-index: 1;">
+                <h2 class="flex items-center text-2xl font-serif font-bold tracking-widest mb-4">
+                    <span class="mr-3 text-3xl text-blue-500"><i class="fab fa-twitter"></i></span>
+                    <a href="https://twitter.com/studioshonos" target="_blank">@studioshonos</a>
+                </h2>
+                <h2 class="flex items-center text-2xl font-serif font-bold tracking-widest mb-4">
+                    <span class="mr-3 text-3xl text-pink-500"><i class="fab fa-instagram"></i></span>
+                    <a href="https://www.instagram.com/studio.shonos/" target="_blank">@studioshonos</a>
+                </h2>
+                <h2 class="flex items-center text-2xl font-serif font-bold tracking-widest mb-4">
+                    <span class="mr-3 text-3xl text-purple-500"><i class="fab fa-discord"></i></span>
+                    <a href="https://discord.com/invite/h9BjMyu6" target="_blank">discord.com/h9BjMyu6</a>
+                </h2>
+                <h2 class="flex items-center text-2xl font-serif font-bold tracking-widest mb-4">
+                    <span class="mr-3 text-3xl text-red-500"><i class="fab fa-youtube"></i></span>
+                    <a href="https://www.youtube.com/@StudioShonos" target="_blank">@StudioShonos</a>
+                </h2>
+                <h2 class="flex items-center text-2xl font-serif font-bold tracking-widest">
+                    <span class="mr-3 text-3xl text-yellow-500"><i class="far fa-envelope"></i></span>
+                    studioshonos@gmail.com
+                </h2>
+            </div>
+            <div class="absolute top-0 transform -translate-x-[21rem] -translate-y-[4rem]">
+                <img src="public/assets/contact/1.png" alt="Imagen 1"
+                    class="h-52 rounded-md border-gray-700 shadow-xl opacity-85">
+            </div>
+            <div class="absolute top-0 right-0 transform -translate-x-[28rem] -translate-y-[7rem]">
+                <img src="public/assets/contact/2.png" alt="Imagen 2"
+                    class="h-52 rounded-md border-gray-700 shadow-xl opacity-85">
+            </div>
+            <div class="absolute bottom-0 transform translate-y-[10rem] -translate-x-[19rem]">
+                <img src="public/assets/contact/3.png" alt="Imagen 3"
+                    class="h-56 rounded-md border-gray-700 shadow-xl opacity-85">
+            </div>
+            <div class="absolute bottom-0 right-0 transform -translate-x-[29rem] translate-y-[7rem]">
+                <img src="public/assets/contact/4.png" alt="Imagen 4"
+                    class="h-52 rounded-md border-gray-700 shadow-xl opacity-85">
+            </div>
+        </div>
+        <div class="mt-9 py-9"></div>
     </div>
 </template>
-
-<script>
-export default {
-    setup () {
-        
-
-        return {}
-    }
-}
-</script>
-
-<style lang="scss" scoped>
-
-</style>
