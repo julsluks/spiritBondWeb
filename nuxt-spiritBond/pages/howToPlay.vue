65c2fd9aad9f0ee3e437d3e8fc374845f8cb6994
julsluks <a22betvilver@inspedralbes.cat>
2024-05-12 17:20:26 +0200

    feat: Mejorar diseño y contenido de la página "Cómo Jugar"

--- nuxt-spiritBond/pages/howToPlay.vue
+++ nuxt-spiritBond/pages/howToPlay.vue
@@ -1,9 +1,11 @@
 <template>
     <div
         class="min-h-screen w-full bg-gradient-to-br from-blue-600 to-blue-950 flex flex-col items-center justify-center text-white">
-        <h1 class="text-6xl font-serif font-bold tracking-widest mb-4"
+        <div class="mt-9 pt-9">
+            <h1 class="text-6xl font-serif font-bold tracking-widest mb-4 text-center mt-9"
             style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">HOW TO PLAY</h1>
-        <div class="min-h-1/2 w-full mt-9">
+        </div>
+        <div class="min-h-1/2 w-full mt-6 mb-4">
             <div class="flex flex-col items-center justify-center m-4">
                 <p class="text-lg tracking-widest mb-4 text-center bg-blue-950 bg-opacity-30 p-5 rounded-lg">
                     It's played using the keyboard. With the
@@ -11,6 +13,9 @@
                     character's action from walking to running. <br> The space bar is for jumping, with the “E” key we
                     interact with the objects in the video game,<br> while with the “Q” we summon the sister and crouch with
                     the control,<br> we also want to implement an Xbox controller.
+                </p>
+                <p class="text-lg tracking-widest mb-4 text-center bg-blue-950 bg-opacity-30 p-5 rounded-lg">
+                    To play with a controller, the square is used to interact and the <br> “X” is used to jump, and the left joystick is used to walk/run.
                 </p>
             </div>
             <div class="lg:flex lg:flex-inline items-center justify-center mt-9">
