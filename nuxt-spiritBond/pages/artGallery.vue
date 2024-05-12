ad644d3c316bc7833722ef509a6087233414789e
julsluks <a22betvilver@inspedralbes.cat>
2024-05-12 17:16:53 +0200

    feat: Update art gallery page layout and images

--- nuxt-spiritBond/pages/artGallery.vue
+++ nuxt-spiritBond/pages/artGallery.vue
@@ -1,19 +1,33 @@
 <template>
-    <div>
-
+    <div class="min-h-screen w-full bg-gradient-to-br from-blue-600 to-blue-950 flex flex-col items-center justify-center text-white">
+        <div class="mt-9 py-9">
+            <h1 class="mt-9 py-9 text-6xl font-serif font-bold tracking-widest mb-4 text-center" style="text-shadow: 5px 3px 4px rgba(255, 255, 255, 0.9);">GALLERY ART</h1>
+        </div>
+        <div class="flex flex-col items-center justify-center aling-center">
+            <iframe class="lg:min-w-[610px] lg:min-h-[365px] md:min-w-[410px] md:min-h-[250px] mx-7 my-4" src="https://www.youtube-nocookie.com/embed/GSCIoTOiPsc?si=cxAvB6M3amRQvpW-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-6 mb-7 p-5 border">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/1.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/2.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/3.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/4.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/5.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/6.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/7.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/8.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/9.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/10.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/11.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/12.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/13.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/14.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/15.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/16.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/17.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/18.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/19.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/20.png" alt="">
+                <img class="w-[350px] h-[250px] object-cover" src="public/assets/artGallery/Pantalla_de_inicio.png" alt="">
+            </div>
+        </div>
     </div>
 </template>
-
-<script>
-export default {
-    setup () {
-        
-
-        return {}
-    }
-}
-</script>
-
-<style lang="scss" scoped>
-
-</style>
